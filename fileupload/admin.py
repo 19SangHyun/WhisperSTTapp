912577bd6366ab601cb8674e4cd23f58b4bd9ba6
2sangguhyun <hsh002819@gmail.com>
2024-03-30 15:34:54 +0900

    whisper, clova stt on application

--- fileupload/admin.py
+++ fileupload/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Post
+from .models import FileUpload  # 모델이 위치한 경로에 따라 수정해야 할 수 있습니다.
 
-admin.site.register(Post)
+admin.site.register(FileUpload)
--- fileupload/urls.py
+++ fileupload/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
+    #path('', views.fileUpload, name='fileUpload'),
+    path('whisper', views.upload_and_transcribe, name='upload_and_transcribe'),
+    path('clova', views.upload_and_clovatranscribe, name='upload_and_clovatranscribe'),
+=======
     path('', views.fileUpload, name='fileupload'),
+>>>>>>> parent of 1e8c662 (whisper api on app)
 ]
